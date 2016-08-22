4b4f27fb3a9cae152e487c6949fdf5667eaf3109
John Freeman <john.freeman.opensource@gmail.com>
2016-08-22 18:54:56 +0100

    Switched to use Molecule to test role (#14)
    Makes it easy to test locally during development.
    
    Enhancement: resolves #11

---
+++ tests/test_role.py
@@ -0,0 +1,14 @@
+def test_keyboard_file(File):
+    kb = File('/etc/default/keyboard')
+
+    assert kb.exists
+    assert kb.is_file
+    assert kb.user == 'root'
+    assert kb.group == 'root'
+    assert oct(kb.mode) == '0644'
+
+    assert kb.contains('XKBMODEL="pc105"')
+    assert kb.contains('XKBLAYOUT="brai"')
+    assert kb.contains('XKBVARIANT="right_hand"')
+    assert kb.contains('XKBOPTIONS="lv3:alt_switch,compose:rctrl"')
+    assert kb.contains('BACKSPACE="guess"')
